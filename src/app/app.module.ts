d71ee6c28dd53f7aa8b252c9bd3019fab84d0843
nikhilp <reddynikhil.p@gmail.com>
2018-08-10 17:44:13 +0530

    Added components for login,customers,admin,tickets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,89 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import {CustomerService} from './service/customer.service';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { CanActivateAuthGuard } from './can-activate.authguard';
+import { AuthenticationService } from './service/authentication.service';
+import { ServiceTicketComponent } from './service-ticket/service-ticket.component';
+import { ViewServiceComponent } from './view-service/view-service.component';
+import { UserComponent } from './user/user.component';
+import { CustomerComponent } from './customer/customer.component';
+import { DataTablesModule } from 'angular-datatables';
+
+
+
+const routes: Routes = [
+
+  {
+    path: 'login', 
+    component: LoginComponent
+   },
+
+  {
+     path: 'user', 
+     component: UserComponent,
+     canActivate: [CanActivateAuthGuard]
+    },
+
+    {
+      path: 'customer', 
+      component: CustomerComponent,
+      canActivate: [CanActivateAuthGuard]
+     },
+    
+     {
+      path: 'serviceticket', 
+      component: ServiceTicketComponent,
+      canActivate: [CanActivateAuthGuard]
+     },
+     
+     {
+      path: 'viewService', 
+      component: ViewServiceComponent,
+      canActivate: [CanActivateAuthGuard]
+     },
+
+    {
+      path: 'logout',
+      component: LogoutComponent,
+      canActivate: [CanActivateAuthGuard]
+    },
+  {
+    path:'',
+    redirectTo:'user',
+    pathMatch: 'full'
+  }
+];
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    UserComponent,
+    ServiceTicketComponent,
+    ViewServiceComponent,
+    CustomerComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,    
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
+    DataTablesModule
   ],
-  providers: [],
+  providers: [CustomerService,CanActivateAuthGuard, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
